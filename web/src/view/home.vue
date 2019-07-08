578928a8a352cac8ea7509695f9365fa54b33e91
tys <842847519@qq.com>
2019-07-08 09:00:12 +0800

    7.8日考

---
+++ web/src/view/home.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+         <dl v-for="(item,index) in list" :key="index">
+             <dt>{{item.title}}</dt>
+             <dd>{{item.wether}}</dd>
+         </dl>
+    </div>
+</template>
+<script>
+import axios from 'axios'
+import '../data/mock'
+export default {
+    props:{
+
+    },
+    components:{
+
+    },
+    data(){
+        return {
+          list:[]
+        }
+    },
+    computed:{
+
+    },
+    methods:{
+
+    },
+    created(){
+     axios.get('/api/getlist').then((res)=>{
+     console.log(res.data.data);
+     this.list=res.data.data
+     }) 
+    },
+    mounted(){
+
+    }
+}
+</script>
+<style scoped lang="">
+
+</style>
